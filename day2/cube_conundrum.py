9a2348517581901f470f7fd47ce0f85ca48c3039
Laura Reese <laurareese03@gmail.com>
2023-12-02 18:14:39 -0600

    Day 2 part b and code cleanup

--- day2/cube_conundrum.py
+++ day2/cube_conundrum.py
@@ -3,7 +3,6 @@ import re
 with open('game_results.txt') as f:
   lines = f.readlines()
 
-games = {}
 maxes = {
   'red': 12,
   'green': 13,
@@ -13,17 +12,27 @@ maxes = {
 # MORE regexes?? in this economy?? more likely than you think
 valid_games = 0
 game_num = 0
+total = 0
 for line in lines:
+  game_is_valid = True
   game_num += 1
+  power = 1
+
   for color in maxes:
     pattern = re.compile = (r'\d+(?= ' + color + r')')
     selections = [int(x) for x in re.findall(pattern, line)]
-    total = sum(selections)
 
+    # part a work
     if any(i > maxes[color] for i in selections):
-      break
+      game_is_valid = False
 
-    if color == 'blue':
+    if color == 'blue' and game_is_valid:
       valid_games += game_num
 
-print(valid_games)
+    # part b work
+    max_val = max(selections)
+    power *= max_val 
+
+  total += power
+
+print(valid_games, total) # part a, part b

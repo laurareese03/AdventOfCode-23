2dc3de010c877eff5a9f8b59440ef931cece05d5
Laura Reese <laurareese03@gmail.com>
2023-12-05 22:12:12 -0600

    misc: remove all problem inputs

--- day4/scratchcards.py
+++ day4/scratchcards.py
@@ -18,12 +18,12 @@ print(worth)
 
 # part b
 total_cards = 0
-card_multiples = [1] * len(lines) # stores how many of each future card we have. start with 1 og, with more to be
+card_multiples = [1] * len(lines) # stores how many of each future card we have. start with 1 og, with more to be added
 for line in lines:
   current_cards = card_multiples.pop(0)
   # for each original card, we have to add the number of current card duplicates to our total
   total_cards += current_cards
-  # for each duplicate card we have we have to add multiples of [matching numbers (intersect)] number of future cards
+  # for each duplicate card we have we have to add the multiples of [matching numbers (intersect)] number of future cards
   card_wins = get_set_intersects(line)
   added_cards = [current_cards] * card_wins
   card_multiples = [i+j for i,j in itertools.zip_longest(card_multiples, added_cards,fillvalue = 0)]

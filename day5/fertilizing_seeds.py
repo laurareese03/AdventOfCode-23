d1a100930423b3dd53891eb5e2c3efb937892619
Laura Reese <laurareese03@gmail.com>
2023-12-05 21:53:38 -0600

    day 5 part b: rip the brute force method

--- day5/fertilizing_seeds.py
+++ day5/fertilizing_seeds.py
@@ -2,8 +2,8 @@ lines = open('alminac.txt').read()
 lines = lines.split('\n')
 
 seeds = [int(x) for x in lines[0][7:].split(' ')]
-
 conversions = []
+
 for i in range(2,len(lines)):
   line = lines[i].split(' ')
   if line[0] == '':
@@ -23,3 +23,35 @@ for i in range(2,len(lines)):
       holder = 0
   
 print(min(seeds))
+
+seeds = lines[0][7:].split(' ')
+seed_pairs = [[int(seeds[i]), int(seeds[i+1])] for i in range(0, len(seeds), 2)]
+location = 56931750 # approximation from running seed +500000
+seed = 56931750
+conversions = []
+done = False
+# we've got to go back! (backwards, that is)
+while not done: # god i h8 it here
+  for i in range(len(lines)-1, 0, -1):
+    line = lines[i].split(' ')
+    if line[0] == '':
+      for conversion in conversions:
+        if location >= conversion[0] and location <= conversion[0]+conversion[2]:
+          offset = location - conversion[0]
+          location = conversion[1] + offset
+          break
+      # refigure indexes here
+      conversions = []
+    else:
+      try:
+        line = [int(x) for x in line]
+        conversions.append(line)
+      except:
+        holder = 0
+  for pair in seed_pairs:
+    if location >= pair[0] and location < pair[0]+pair[1]:
+      done = True
+  seed += 1 # originally ran by +500000 to get close, then narrowed it down to one when it hit a result
+  location = seed
+
+print(seed-1)

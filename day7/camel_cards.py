e975016058c9b7aa8608c519d53cadd0097f3fa4
Laura Reese <laurareese03@gmail.com>
2023-12-07 19:12:28 -0600

    day 7 part b: edge cases my beloathed

--- day7/camel_cards.py
+++ day7/camel_cards.py
@@ -24,26 +24,34 @@ bids = {}
 def get_highest_count(hand, cards):
   counts = []
   for card in cards:
-    counts.append(hand.count(card))
-  return max(counts)
+    if card != 'J':
+      counts.append(hand.count(card))
+  highest_group = max(counts) + hand.count('J')
+  return highest_group
 
 def get_hand_type(line):
   hand = line[0]
   cards = set(hand)
+  diff_cards = len(cards)
 
-  if len(cards) == 1:
+  # it is absolutely cruel that the only 5 of a kind in the input is JJJJJ
+  # we #livelaughlove tricky edge cases :)))))))
+  if 'J' in hand and hand != 'JJJJJ':
+    diff_cards -= 1
+
+  if diff_cards == 1:
     plays['five'].append(hand)
-  elif len(cards) == 2:
+  elif diff_cards == 2:
     if get_highest_count(hand, cards) == 4:
       plays['four'].append(hand)
     else:
       plays['full'].append(hand)
-  elif len(cards) == 3:
+  elif diff_cards == 3:
     if get_highest_count(hand, cards) == 3:
       plays['three'].append(hand)
     else:
       plays['two'].append(hand)
-  elif len(cards) == 4:
+  elif diff_cards == 4:
     plays['one'].append(hand)
   else:
     plays['high'].append(hand)
@@ -58,7 +66,7 @@ for line in lines:
 current_rank = len(lines)
 total = 0
 for play in plays:
-  alphabet = "AKQJT98765432"
+  alphabet = "AKQT98765432J"
 
   # custom sorting per 
   # https://stackoverflow.com/questions/26579392/sorting-string-values-according-to-a-custom-alphabet-in-python 

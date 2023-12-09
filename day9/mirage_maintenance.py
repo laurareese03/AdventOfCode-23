5b3357a7ff98947bb5c99cb41af3b19094d61899
Laura Reese <laurareese03@gmail.com>
2023-12-09 01:35:38 -0600

    day 9 part b: stop complicating the problem before you get it laura

--- day9/mirage_maintenance.py
+++ day9/mirage_maintenance.py
@@ -6,18 +6,18 @@ lines = open('history.txt').read().split('\n')
 # to reflect nested compounding algebraic series?
 
 # recursion!!! yay!!!
+# there has to be a nonrecursive solution to this.
+# maybe I'll do a little more thinking, or maybe consult reddit
+# but either way its quicker to go this way now
 def get_next_sequence(sequence):
-  print(sequence)
   if len(set(sequence)) == 1:
-    return sequence[-1]
+    return sequence[0]
   new_sequence = []
   for i in range(len(sequence)-1):
     new_sequence.append(int(sequence[i+1]) - int(sequence[i]))
-  return int(sequence[-1]) + get_next_sequence(new_sequence)
+  return int(sequence[0]) - get_next_sequence(new_sequence)
 
 total = 0
 for line in lines:
   total += get_next_sequence(line.split())
 print(total)
-
-# 11865661 low

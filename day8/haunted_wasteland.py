016b0ecad1eb7c51e89985233dee8d7f6b9a8902
Laura Reese <laurareese03@gmail.com>
2023-12-08 20:28:30 -0600

    day 8 part b: love lcms :)

--- day8/haunted_wasteland.py
+++ day8/haunted_wasteland.py
@@ -1,4 +1,5 @@
 import re
+from math import gcd
 
 readed = open('network.txt').read()
 lines = readed.split('\n')
@@ -9,31 +10,33 @@ lines.pop(0)
 # some part b questions to ponder
 # do they end up at different nodes by the end? or will 2 A nodes map to the same Z node?
 
-# there is a (potentially) clever solution to figure out how each node loops and find the lcd between all of them
+# there is a (potentially) clever solution to figure out how each node loops and find the lcm between all of them
 # based on how long the LR string is I'm guessing that's not as clever as I think it is
 # we're gonna try the option that matches with the part a code first tbh
 
-# build the network!
+# hey @ past laura! the idea WAS clever! brute forcing sucks!
+
 network = {}
+# build the network!
 for line in lines:
   pattern = re.compile(r'[A-Z]{3}')
   nodes = re.findall(pattern, line)
   network[nodes[0]] = [nodes[1], nodes[2]]
 
-# will return true if all nodes end in Z
-def evaluate_nodes(list):
-  for n in list:
-    if n[-1] != 'Z':
-      return False
-  return True
-
 # recursion!!! no!!!
-count = 0
 node_list = re.findall(r'[A-Z]{2}A', readed)
-while not evaluate_nodes(node_list):
-  node_list = [network[node][int(directions[count % len(directions)])] for node in node_list]
-  count += 1
-  if count % 10000 == 0:
-    print(count)
+counts = []
+for node in node_list:
+  count = 0
+  while node[-1] != 'Z':
+    node = network[node][int(directions[count % len(directions)])]
+    count += 1
+  # VERY fortunate that the first time we get to a node ending in Z, the complete cycle repeats
+  counts.append(count)
 
-print(count)
+# least common multiple for n inputs from
+# https://stackoverflow.com/questions/37237954/calculate-the-lcm-of-a-list-of-given-numbers-in-python
+lcm = 1
+for i in counts:
+    lcm = lcm*i//gcd(lcm, i)
+print(lcm)

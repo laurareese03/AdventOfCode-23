5dee48b25dd057b7acba2cd6bb6b281933628560
Laura Reese <laurareese03@gmail.com>
2023-12-14 19:32:41 -0600

    day 14 part b: repeating pattern problems are my faves

--- day14/parabolic_reflector_dish.py
+++ day14/parabolic_reflector_dish.py
@@ -3,21 +3,32 @@ import numpy as np, re
 lines = open('platform.txt').read().split('\n')
 platform = np.stack([list(line) for line in lines], dtype=object)
 
-# shift the rocks!
-for j in range(len(platform[0])):
-  column_string = ''.join(str(x) for x in platform[:,j])
-  rocks = column_string.split('#')
-  
-  for i in range(len(rocks)):
-    rocks[i] = list(rocks[i])
-    rocks[i].sort(reverse=True)
-    rocks[i] = ''.join(str(x) for x in rocks[i])
-  column_string = '#'.join(str(rock) for rock in rocks)
-  platform[:,j] = list(column_string)
+for l in range(500):
+  # complete 4 turns, 1 cycle
+  for k in range(4):
+    # shift the rocks!
+    for j in range(len(platform[0])):
+      column_string = ''.join(str(x) for x in platform[:,j])
+      rocks = column_string.split('#')
+      
+      for i in range(len(rocks)):
+        rocks[i] = list(rocks[i])
+        rocks[i].sort(reverse=True)
+        rocks[i] = ''.join(str(x) for x in rocks[i])
+      column_string = '#'.join(str(rock) for rock in rocks)
+      platform[:,j] = list(column_string)
+    platform = np.rot90(platform, axes=(1,0))
 
-# count the rocks!
-total_weight = 0
-for i in range(len(platform)):
-  row_count = np.char.count(np.array_str(platform[i,:]), 'O')
-  total_weight += row_count * (len(platform)-i)
+  # count the rocks!
+  total_weight = 0
+  for i in range(len(platform)):
+    row_count = np.char.count(np.array_str(platform[i,:]), 'O')
+    total_weight += row_count * (len(platform)-i)
+  print(total_weight)
 print(total_weight)
+
+# pattern repeating problem!
+# takes FAR too long to brute force, but eventually the platform stablizes after a certain number of cycles
+# we let the program run for a little while to figure out the pattern in platform weights
+# once we have the pattern, we take (wanted number of cycles - number of pre-stable cycles) % cycle pattern length.
+# the answer for this gives us the index in the pattern which contains the weight for the billionth cycle :D

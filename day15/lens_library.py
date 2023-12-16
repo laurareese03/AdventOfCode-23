270ddf26b31dee658b00e88fcb82bc178950f848
Laura Reese <laurareese03@gmail.com>
2023-12-15 19:08:14 -0600

    day 15 part b: thank god no numpy was needed today

--- day15/lens_library.py
+++ day15/lens_library.py
@@ -8,8 +8,23 @@ def get_hash_output(str):
     result %= 256
   return result
 
-total = 0
+boxes = [{}.copy() for i in range(256)]
 for line in lines:
-  result = get_hash_output(line)
-  total += result
-print(total)
+  if line[-1] != '-':
+    line = line.split('=')
+    result = get_hash_output(line[0])
+    boxes[result][line[0]] = line[1]
+  else:
+    box = get_hash_output(line[:-1])
+    try:
+      del boxes[box][line[:-1]]
+    except:
+      here = True
+
+focusing_power = 0
+for i in range(len(boxes)):
+  slot = 1
+  for lens in boxes[i]:
+    focusing_power += (i+1) * slot * int(boxes[i][lens])
+    slot += 1
+print(focusing_power)

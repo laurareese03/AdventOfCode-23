6bddad476e7a176ae8acadc2ca7128b642c181b3
Laura Reese <laurareese03@gmail.com>
2024-12-01 22:47:04 -0600

    post-event: finish 18.b

--- day18/lavaduct_lagoon.py
+++ day18/lavaduct_lagoon.py
@@ -3,7 +3,6 @@ import numpy as np, queue
 lines = open('dig_plan.txt').read().split('\n')
 
 points = [(0,0)]
-
 perimeter = 0
 for line in lines:
   line = line.split(' ')
@@ -21,32 +20,47 @@ for line in lines:
       next_point = (last_point[0], last_point[1]+1)
     points.append(next_point)
 
-x_min=0
-y_min=0
-x_max=0
-y_max=0
-for point in points:
-  x_min = min(x_min, point[0])
-  y_min = min(y_min, point[1])
-  x_max = max(x_max, point[0])
-  y_max = max(y_max, point[1])
-print(x_min, y_min, x_max, y_max)
-
-# flood fill :|
+# # flood fill :|
 frontier = queue.Queue()
 frontier.put((-1,-1))
 while not frontier.empty():
   point = frontier.get()
-  print(point)
   if point in points:
-    print('here?', len(points))
     continue
   points.append(point)
   frontier.put((point[0]+1, point[1]))
   frontier.put((point[0], point[1]+1))
   frontier.put((point[0]-1, point[1]))
   frontier.put((point[0], point[1]-1))
-
-print(points)
-
 print(len(set(points)))
+
+points = [(0,0)]
+directions = ['R', 'D', 'L', 'U']
+perimeter = 0
+for line in lines:
+  line = line.split(' ')[2]
+  distance = int(line[2:-2], 16)
+  direction = directions[int(line[-2])]
+  last_point = points[-1]
+  new_point = ()
+  if direction == 'R':
+    new_point = (last_point[0], last_point[1]+distance)
+  elif direction == 'U':
+    new_point = (last_point[0]-distance, last_point[1])
+  elif direction == 'L':
+    new_point = (last_point[0], last_point[1]-distance)
+  else: # direction == 'D'
+    new_point = (last_point[0]+distance, last_point[1])
+  perimeter += distance
+  points.append(new_point)
+
+# weird combo of pick's & shoelace
+# find the area using shoelace, and use that to calculate number of interior points using pick's
+# add interior points to perimeter to get total volume
+total = 0
+for i in range(len(points)-1):
+  total += np.linalg.det((points[i], points[i+1]))
+
+area = round(abs(total)/2) # can't use int() here! learned the hard way!
+interior_points = (area + 1) - int(perimeter/2)
+print(interior_points + perimeter)
